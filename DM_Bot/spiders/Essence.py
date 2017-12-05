fc82147401e9bfa37dd155e930097ca6e35d2e27
Frank H.J. Du <Frankdew1995@users.noreply.github.com>
2017-12-05 10:18:36 +0100

    Add files via upload

--- DM_Bot/spiders/Essence.py
+++ DM_Bot/spiders/Essence.py
@@ -24,14 +24,15 @@ class DmspiderSpider(scrapy.Spider):
             item['Price'] = product['price']
             yield item
         api_url = "https://services.dm.de/websearch/search/pues?type=product&tenant=de_mcr&q=essence&categoryId=&pageSize=48&sort=relevance&cp={}&productQuery=&initialProductQuery=&hiddenFacets="
-        # page = 1
-        for i in range(2,27):
-            next_page = i
-            next_url = api_url.format(next_page)
-            yield scrapy.Request(url = next_url, callback = self.parse)
-        # if data['serviceProducts'] != []:
-        #     next_url = api_url.format(page + 1)
+
+        # for i in range(2,27):
+        #     next_page = i
+        #     next_url = api_url.format(next_page)
         #     yield scrapy.Request(url = next_url, callback = self.parse)
+        if len(data['serviceProducts']) == 48 :
+            page = 1
+            next_url = api_url.format(page + 1)
+            yield scrapy.Request(url = next_url, callback = self.parse)
 
 
 
